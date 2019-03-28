7364b8c18a9a6451f9244ca1875637207fd18c04
fimi.zhuang <725459159@qq.com>
2019-03-28 13:37:56 +0800

    Update ContentPublishPage.vue

--- src/components/content/ContentPublishPage.vue
+++ src/components/content/ContentPublishPage.vue
@@ -3,10 +3,10 @@
     <Divider>内容发布</Divider>
     <Form ref="formRef" :model="contentData" :rules="ruleValidate" :label-width="80">
       <FormItem label="标题" prop="title">
-        <Input v-model="contentData.title" placeholder="2-80字符"></Input>
+        <Input v-model="contentData.title" placeholder="2-80字符" style="width: 400px"></Input>
       </FormItem>
       <FormItem label="摘要" prop="summary">
-        <Input v-model="contentData.summary" placeholder="2-140字符"></Input>
+        <Input v-model="contentData.summary" placeholder="2-140字符" style="width: 400px"></Input>
       </FormItem>
       <FormItem label="图片" prop="imageType">
         <RadioGroup v-model="contentData.imageType">
@@ -14,20 +14,23 @@
           <Radio label="1">本地上传</Radio>
         </RadioGroup>
       </FormItem>
-      <FormItem label="City" prop="city">
-        <Select v-model="contentData.city" placeholder="Select your city">
-          <Option value="beijing">New York</Option>
-          <Option value="shanghai">London</Option>
-          <Option value="shenzhen">Sydney</Option>
-        </Select>
+      <FormItem label="图片地址" prop="imagePath">
+        <Input v-model="contentData.imagePath" placeholder="图片地址" style="width: 400px"></Input>
       </FormItem>
-      <FormItem label="Desc" prop="desc">
-        <Input v-model="contentData.desc" type="textarea" :autosize="{minRows: 2,maxRows: 5}"
-               placeholder="Enter something..."></Input>
+      <FormItem label="本地图片" prop="imagePath">
+        <Upload action="">
+          <Button icon="ios-cloud-upload-outline">选择本地图片</Button>
+        </Upload>
+      </FormItem>
+      <FormItem label="正文" prop="detail">
+        <Input v-model="contentData.detail" type="textarea" :autosize="{minRows: 4,maxRows: 8}" placeholder="2-1000字符" style="width: 400px" ></Input>
+      </FormItem>
+      <FormItem label="价格" prop="price">
+        <Input v-model="contentData.price" type="number" style="width: 200px"  ></Input>
       </FormItem>
       <FormItem>
-        <Button type="primary" @click="handleSubmit('formRef')">Submit</Button>
-        <Button @click="handleReset('formRef')" style="margin-left: 8px">Reset</Button>
+        <Button type="primary" @click="handleSubmit('formRef')">提交</Button>
+        <Button @click="handleReset('formRef')" style="margin-left: 8px">重置</Button>
       </FormItem>
     </Form>
   </div>
